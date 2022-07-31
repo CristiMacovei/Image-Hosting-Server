4c1c9de738bbb25c6120b89f80932c54a3428523
CristiMacovei <64579555+CristiMacovei@users.noreply.github.com>
2022-07-31 14:22:54 +0300

    upload url done

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const fs = require('fs/promises');
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const b58 = require('base-58');
 
 const { getSequence, saveSequence } = require('./utils/sequence');
+const { default: axios } = require('axios');
 
 const root = path.join(__dirname);
 
@@ -11,16 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-let sequence = null;
-
 async function main() {
-  sequence = await getSequence();
-  
-  if (sequence === null) {
-    console.log('Error when parsing sequence');
-    return;
-  }
-
   app.listen(11983, () => {
     console.log('🚀 Running on rocket fuel');
   })
@@ -31,6 +24,42 @@ main();
 app.post('/upload', async (req, res) => {
   // todo
 });
+
+app.post('/upload-url', async (req, res) => {
+  const url = req.body.url;
+  const type = req.body.type;
+
+  if (!type.startsWith('image/png')) {
+    res.json({
+      'status': 'error',
+      'error': {
+        'message': `Unsupported type ${type}`
+      }
+    });
+
+    return;
+  }
+
+  const fileExtension = type.substring('image/'.length);
+
+  const response = await axios.get(url, {
+    responseType: 'arraybuffer'
+  });
+
+  const sequence = await getSequence();
+
+  const fileName = b58.encode(Buffer.from(sequence.toString())) + '.' + fileExtension;
+  const filePath = path.join(root, 'uploads', fileName);
+
+  await fs.writeFile(filePath, response.data);
+
+  saveSequence(sequence + 1);
+
+  res.json({
+    'status': 'success',
+    'url': `${process.env.SERVER_URL}${fileName}`
+  });
+})
 
 app.get('/:name', async (req, res) => {
   res.sendFile(path.join(root, 'uploads', req.params.name));
